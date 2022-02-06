6d732ab997f07d2777f55a26735f3fc2d76f7d89
Tee-Mureithi <esther.mureithi@student.moringaschool.com>
2022-02-07 00:32:03 +0300

    will display quotes

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { QuotePhrase } from '../quote-phrase';
 
 @Component({
   selector: 'app-landing-page',
@@ -6,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing-page.component.css']
 })
 export class LandingPageComponent implements OnInit {
+
+  quotes? : QuotePhrase[] = [
+    new QuotePhrase(1, 'If you want to live a happy life, tie it to a goal, not to people or things','Babe Ruth','Tee-Mureithi',80,4,new Date(2019,0,18)),
+
+    new QuotePhrase(2,'Money and success don’t change people; they merely amplify what is already there.','Will Smith',
+    'Habiba Ali',50,2,new Date(2021,3,12)),
+
+    new QuotePhrase(3,'Not how long, but how well you have lived is the main thing.','Alber Einstein','Josh Kamau',30,4,new Date (2021,8,11))
+
+  ]
 
   constructor() { }
 
